056a282a74196b0d49b1572e34ed94212ece1a18
beeb <vbersier@gmail.com>
2022-12-01 21:42:35 +0100

    feat: time is milliseconds

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -31,35 +31,41 @@ pub mod day24;
 pub mod day25;
 
 pub trait Day {
-  type Input;
+    type Input;
 
-  fn parse(input_string: &str) -> IResult<&str, Self::Input>;
+    fn parse(input_string: &str) -> IResult<&str, Self::Input>;
 
-  type Output1: Display;
+    type Output1: Display;
 
-  fn part_1(input: &Self::Input) -> Self::Output1;
+    fn part_1(input: &Self::Input) -> Self::Output1;
 
-  type Output2: Display;
+    type Output2: Display;
 
-  fn part_2(input: &Self::Input) -> Self::Output2;
+    fn part_2(input: &Self::Input) -> Self::Output2;
 
-  fn parse_file(fp: &str) -> Result<Self::Input, MyErr> {
-    let input_string = read_to_string(fp)?;
-    let (_, input) = Self::parse(&input_string)?;
-    Ok(input)
-  }
-
-  fn run_day(fp: &str) {
-    match Self::parse_file(fp) {
-      Err(e) => println!("{:?}", e),
-      Ok(input) => {
-        let before1 = Instant::now();
-        println!("Part 1: {}", Self::part_1(&input));
-        println!("Part 1 took {}s", before1.elapsed().as_secs_f32());
-        let before2 = Instant::now();
-        println!("Part 2: {}", Self::part_2(&input));
-        println!("Part 2 took {}s", before2.elapsed().as_secs_f32());
-      }
+    fn parse_file(fp: &str) -> Result<Self::Input, MyErr> {
+        let input_string = read_to_string(fp)?;
+        let (_, input) = Self::parse(&input_string)?;
+        Ok(input)
     }
-  }
+
+    fn run_day(fp: &str) {
+        match Self::parse_file(fp) {
+            Err(e) => println!("{:?}", e),
+            Ok(input) => {
+                let before1 = Instant::now();
+                println!("Part 1: {}", Self::part_1(&input));
+                println!(
+                    "Part 1 took {}ms",
+                    before1.elapsed().as_nanos() as f32 / 1e6
+                );
+                let before2 = Instant::now();
+                println!("Part 2: {}", Self::part_2(&input));
+                println!(
+                    "Part 2 took {}ms",
+                    before2.elapsed().as_nanos() as f32 / 1e6
+                );
+            }
+        }
+    }
 }
