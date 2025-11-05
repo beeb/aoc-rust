1508d711ecef0ffa1acf92b3a44e7dc2d1fa899e
beeb <703631+beeb@users.noreply.github.com>
2025-11-05 14:08:09 +0100

    chore(flake): update flake

--- flake.nix
+++ flake.nix
@@ -1,34 +1,34 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    # Rust
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        stdenv = if pkgs.stdenv.isLinux then pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv else pkgs.stdenv;
-      in
-      {
-        devShells.default = pkgs.mkShell.override { inherit stdenv; } {
-          buildInputs = [
-            pkgs.rust-analyzer-unwrapped
-            toolchain
-          ];
-          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-        };
-      });
+  outputs = { self, nixpkgs, fenix }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlays.default ];
+          };
+          toolchain = fenix.packages.${system}.fromToolchainFile { dir = ./.; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              toolchain
+            ];
+
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+        }
+      );
+    };
 }
